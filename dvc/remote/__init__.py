b7c737a5db03683969e92f6a259791c3604bab44
Aleksey Gurtovoy <agurtovoy@acm.org>
2021-06-02 18:25:24 -0500

    repo: allow user to specify different state and index tmp dir locations
    Allow user to override state's cache and remote's index files location
    through config when, say, the repository is located on a mounted volume.
    
    In relation to #4420

--- dvc/remote/__init__.py
+++ dvc/remote/__init__.py
@@ -7,5 +7,5 @@ def get_remote(repo, **kwargs):
     cls, config, path_info = get_cloud_fs(repo, **kwargs)
     fs = cls(**config)
     if fs.scheme == "local":
-        return LocalRemote(fs, path_info, repo.tmp_dir, **config)
-    return Remote(fs, path_info, repo.tmp_dir, **config)
+        return LocalRemote(fs, path_info, repo.index_tmp_dir, **config)
+    return Remote(fs, path_info, repo.index_tmp_dir, **config)
